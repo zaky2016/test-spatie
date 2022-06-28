f99281df4b17a12a116096d9845cb5deaa91f2ad
unknown <elmeghits.zakarya@gmail.com>
2022-06-28 21:30:23 +0100

    spatie front end

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class HomeController extends Controller
 {
@@ -22,13 +27,42 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
 
-        $user = Auth::user();
+        // Auth::guard('web')->logout();
+        // $request->session()->invalidate();
+        // $request->session()->regenerateToken();
 
-        dd($user);
-        
-        return view('home');
+        $authed = auth()->user();
+
+        // \App\Models\User::find($user)->assignRole(1); User::with('roles')->get()
+        $user = User::find($authed)->first();
+
+        // You can apply assignRole() on a model instance, not a builder
+        // $user->assignRole(['moderator']);
+
+
+        $perm = $user->getAllPermissions();
+        // dd($user->name , $perm->pluck('name')); to get specifc key from collection of arrays
+
+        $all = Role::with('permissions')->get();
+        //dd($all); //[0]->permissions[0]->name
+
+        $roles = Role::all();
+        $permissions = Permission::all();
+
+        return view('home', [
+            'user' => $user,
+            'perm' => $perm,
+            'permissions'=>$permissions,
+            'roles'=>$roles,
+            "role_permissions" => Role::with('permissions')->get()
+        ]);
+    }
+
+    public function change(Request $request){
+
+        dd($request->all());
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('currrent_user') }}</div>
 
-                <div class="card-body">
+                    <div class="card-body">
 
-            
+                        <h2>{{ $user->name }}</h2>
+
+                        @foreach ($perm as $item)
+                            <p>{{ $item->name }}</p>
+                        @endforeach
+
+
+                    </div>
+                </div>
+
+
+            <form action="{{url('/change')}}" method="POST">
+                @csrf
+                <div class="row">
+                    @foreach ($roles as $role)
+                        <div class="col">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h1>{{ $role->name }}</h1>
+                                </div>
+
+                                <div class="card-body">
+                                    <input type="text" name="{{ $role->name }}" value="{{ $role->id }} " hidden>
+
+                                    @foreach ($permissions as $p)
+                                        <div class="row">
+                                            <input type="checkbox"  name="{{ $role->name }}[]" value="{{ $p->name }}" />
+                                            <p>{{ $p->name }}</p>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+
+                <div class="row justify-content-md-center">
+                <button class=""type="submit"> save </button>
 
                 </div>
+            </form>
+
             </div>
         </div>
-    </div>
-</div>
-@endsection
+    @endsection
